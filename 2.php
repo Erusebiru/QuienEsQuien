3baecb519c4d91f78a877f360bbb9e2759dd599a
Erusebiru <Erusebiru@gmail.com>
2018-10-16 16:40:33 +0200

    5a terminada

--- 2.php
+++ 2.php
@@ -22,62 +22,52 @@
 			}
 		}
 
-		//Creamos el array de configuración
-
-		$myfile = fopen("config.txt", "r") or die("Unable to open file!");
-		while(!feof($myfile)) {
-			$linea = fgets($myfile);
-			if(!empty(trim($linea))){ //Comprobamos que la línea no esté vacía
-				$lineaExp = explode(':', $linea); //Hacemos split para dividir key de value
-				$lineaExp2 = explode("\r\n",$lineaExp[1]);
-				$configKey = $lineaExp[0]; //Creamos la variable con la key
-				$configValues = explode(';', $lineaExp2[0]); //Hacemos split para crear un array con los valores
-				$config[$configKey] = $configValues; //Asignamos los valores a la key
-			}
-		}
-		fclose($myfile);
-
-
-		$file = file("1.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-		foreach($file as $linea){
-			$img = explode(':',$linea);			
-			$split1 = explode(';', $img[1]); //Mete en un array, por cada vuelta, un array con la línea anterior spliteada por punto y coma
-			foreach($split1 as $value){
-				$split2 = preg_split('/ +/', $value);
-				$carta[$split2[0]] = $split2[1];
-			}
-			$cartasText[$img[0]] = $carta;
-		}
-		/*$myfile = fopen($file, "r") or die("Unable to open file!");
-		while(!feof($myfile)) { //Comprueba que haya algo
-			$linea = fgets($myfile) . "<br>"; //Mete en un string una línea a cada vuelta
-			//$linea2 = explode("\r\n",$linea);
-			$img = explode(':',$linea);
-			
-			$split1 = explode(';', $img[1]); //Mete en un array, por cada vuelta, un array con la línea anterior spliteada por punto y coma
-			foreach($split1 as $value){
-				$split2 = preg_split('/ +/', $value);
-				$carta[$split2[0]] = $split2[1];
-			}
-			$cartasText[$img[0]] = $carta;
-		}
-		fclose($myfile);
-		
-print_r($cartasText);
-
-		//Creamos el array con los datos
-		/*$cartasText = json_decode(file_get_contents('2.txt'), true);
-		$control = true;*/
-
-
-		if(!conf($cartasText,$config)){
+		$config = specFile();
+		$cartasText = specConfig();
+		if($cartasText == false){
 			echo "ERROR";
-		}else if(!equal($cartasText)){
+		}else if(!conf($cartasText,$config)){
 			echo "ERROR1";
+		}else if(!equal($cartasText)){
+			echo "ERROR2";
 		}else{
 			echo "good";
 		}
 
+
+		function specFile(){
+			$myfile = fopen("config.txt", "r") or die("Unable to open file!");
+			while(!feof($myfile)) {
+				$linea = fgets($myfile);
+				if(!empty(trim($linea))){ //Comprobamos que la línea no esté vacía
+					$lineaExp = explode(':', $linea); //Hacemos split para dividir key de value
+					$lineaExp2 = explode("\r\n",$lineaExp[1]);
+					$configKey = $lineaExp[0]; //Creamos la variable con la key
+					$configValues = explode(';', $lineaExp2[0]); //Hacemos split para crear un array con los valores
+					$config[$configKey] = $configValues; //Asignamos los valores a la key
+				}
+			}
+			return $config;
+			fclose($myfile);
+		}
+
+		function specConfig(){
+			$cartasText = [];
+			$file = file("1.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+			foreach($file as $linea){
+				$img = explode(':',$linea);			
+				$split1 = explode(';', $img[1]); //Mete en un array, por cada vuelta, un array con la línea anterior spliteada por punto y coma
+				foreach($split1 as $value){
+					$split2 = preg_split('/ +/', $value);
+					$carta[$split2[0]] = $split2[1];
+				}
+				if(array_key_exists($img[0],$cartasText)){
+					return false;
+				}
+				$cartasText[$img[0]] = $carta;
+			}
+			return $cartasText;
+		}
 
 		function conf($cartasText, $config){
 			foreach($cartasText as $cartaText){
@@ -98,28 +88,6 @@ print_r($cartasText);
 			}
 			return false;
 		}
-		//Función para comprobar que el archivo de datos es correcto y cuadra con el de configuración
-		/*function conf($cartasText,$config){
-			foreach($cartasText as $cartaText){
-				foreach($cartaText as $key => $value){
-					//echo $key; //SI ES *LO QUE SEA* QUE NO ENTRE
-					if(checkConf($value,$config) === false){
-						return false;
-					}
-				}
-				//$cartasObject[] = new Carta($cartaText['id'],null,$cartaText['gafas'],$cartaText['pelo'],$cartaText['genero']);
-			}
-			return true;
-		}
-
-		function checkConf($value,$config){
-			foreach($config as $conf){
-				if(in_array($value,$conf)){
-					return true;
-				}
-			}
-			return false;
-		}*/
 
 		//Función para comprobar que la configuración de las imágenes no está repetida
 		function equal($cartasText){
